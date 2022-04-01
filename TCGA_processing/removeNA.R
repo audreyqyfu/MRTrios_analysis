ec6f764e0899f6ce588df173f989eca8c35cdf7b
banditakarki <91904380+banditakarki@users.noreply.github.com>
2022-03-31 22:26:12 -0700

    Create removeNA.R

---
+++ TCGA_processing/removeNA.R
@@ -0,0 +1,22 @@
+removeNA.meth <- function(TCGA.meth){
+
+   #find the rows that have NA as values for all the individuals in Methylation data
+   na.cols.meth <- which(rowSums(is.na(TCGA.meth[,5:ncol(TCGA.meth)])) == ncol(TCGA.meth[,5:ncol(TCGA.meth)]))
+
+   #return the result
+   return(na.cols.meth)
+
+
+}
+
+
+removeNA.gene <- function(gene.exp){
+
+   #find the rows that have NA as values for all the individuals in Gene Exp data
+   na.cols.gene <- which(rowSums(is.na(gene.exp[,3:ncol(gene.exp)])) == ncol(gene.exp[,3:ncol(gene.exp)]))
+
+   #return the result
+   return(na.cols.gene)
+
+
+}
