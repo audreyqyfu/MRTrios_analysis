f7e02dcc6236cbfcfd88e13b3872facb31c2a0d4
banditakarki <91904380+banditakarki@users.noreply.github.com>
2022-03-31 22:23:57 -0700

    Update findTrioAll.R

--- TCGA_processing/findTrioAll.R
+++ TCGA_processing/findTrioAll.R
@@ -1,83 +1,71 @@
-#set the directory
-setwd("/mnt/ceph/megheib/Cancer_Genomics")
-getwd()
+findTrioAll <- function(meth.data, cna.data, gene.data) {
 
-load("ntrez_id.2.RData")
-master.list <- ntrez_id.2
-  
-#set the directory
-setwd("/mnt/ceph/megheib/Cancer_Genomics/TCGA_data_results")
-getwd()
+#rows with duplicates in CNA data
+dup.CNA <- findDupsCNA(cna.data)
 
-#load the library
-library(data.table)
-library(splitstackshape)
+#rows with duplicates in Gene Exp data
+dup.GENE <- findDupsGENE(gene.data)
 
-#read in the datasets
-gene.exp <- fread("/mnt/ceph/fu_lab/TCGA/cbioportal/brca_tcga/data_RNA_Seq_v2_mRNA_median_all_sample_Zscores.txt")
-dim(gene.exp)
+#rows with NAs for all individual in Methylation data
+na.METH <- removeNA.meth(meth.data)
 
+#rows with NAs for all individual in Gene Exp data
+na.GENE <- removeNA.gene(gene.data)
 
-cna.TCGA <- fread("/mnt/ceph/fu_lab/TCGA/cbioportal/brca_tcga/data_CNA.txt")
-dim(cna.TCGA)
-
-
-load("TCGA.meth.RData")
-dim(TCGA.meth)
-
-
-#counts the number of NAs in each row
-#returns the rows that do not have a sum of NAs that match with the number of columns
-#so basically removes the rows that have all NAs for methylation levels
-
-gene <- gene.exp[rowSums(is.na(gene.exp[,3:1102])) != ncol(gene.exp[,3:1102]), ]
-dim(gene)
-gene[1:5,1:5]
-
-cna <- cna.TCGA[rowSums(is.na(cna.TCGA[,3:1082])) != ncol(cna.TCGA[,3:1082]), ]
-dim(cna)
-cna[1:5,1:5]
-
-meth <- TCGA.meth[rowSums(is.na(TCGA.meth[,5:899])) != ncol(TCGA.meth[,5:899]), ]
-dim(meth)
-meth[1:5,1:5]
-
-
-################################### matching trios ##############
 #since the data has multiple genes in one row separated by ";", we split them
-split.genes <- unlist(strsplit(as.character(meth$Gene_Symbol), ';'))
+split.genes <- unlist(strsplit(as.character(meth.data$Gene_Symbol), ';'))
 
 #then find the unique genes from the methylation data
-unique.genes <- unique(split.genes)
+uni <- na.omit(unique(split.genes))
+
+#remove any empty values in the uni list and skip the duplicated rows in CNA data
+uni2 <- uni[-na.omit(match(cna$Hugo_Symbol[dup.CNA], uni))]
+
+#skip the duplicated rows in Gene Exp data
+unique.genes <- uni2[-na.omit(match(gene$Hugo_Symbol[dup.GENE], uni2))]
 
 #to find the row in the methylation data
-meth.genes <- strsplit(as.character(meth$Gene_Symbol), ';')
+meth.genes <- strsplit(as.character(meth.data$Gene_Symbol), ';')
 
+#initialize the tmp variable
 tmp <- NULL
-findTrioAll <- function(meth.data, cna.data, gene.data, dupsList) {
 
-    for(i in 1:unique.genes){
-    
+    #loop through each gene
+    for(i in 1:length(unique.genes)){
+    print(i)
+
         #apply the function with the provided data
-        tmp <- cbind(tmp,trios(unique.genes[i]))
+        tmp <- rbind(tmp,trios(unique.genes[i], meth.data, cna.data, gene.data, meth.genes, dup.CNA, dup.GENE, na.METH, na.GENE))
+
   }
 
-dim(tmp)
-tmp[1:5,]
+# dim(tmp)
+#print(tmp[1:5,])
 
 #find the rows that have NAs for each data type
 meth.na.rows <- which(is.na(tmp[,2]))
-cna.na.rows <- which(is.na(tmp[,3])) 
+cna.na.rows <- which(is.na(tmp[,3]))
 gene.na.rows <- which(is.na(tmp[,4]))
 
-for(i in 1:length(cna.na.rows){
+#loop through the rows that did not have a gene match in CNA data
+for(i in 1:length(cna.na.rows)){
 
-    entrez.cna(cna.na.rows[i])
+    #find the entrez id match and replace the NA
+    tmp[cna.na.rows[i],3] <- entrez.cna(cna.na.rows[i], tmp, cna.data, gene.data)
 }
 
-for(i in 1:length(gene.na.rows){
+print(tmp[1:5,])
 
-    entrez.cna(gene.na.rows[i])
+#loop through the rows that did not have a gene match in Gene Exp data
+for(i in 1:length(gene.na.rows)){
+
+    #find the entrez id match and replace the NA
+    tmp[gene.na.rows[i],4] <- entrez.gene(gene.na.rows[i], tmp, cna.data, gene.data)
 }
+
+print(tmp[1:5,])
+
+#return the final data
+return(tmp)
 
 }
