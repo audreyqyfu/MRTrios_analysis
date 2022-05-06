e65863d33ef51de83dabbda67213d31bdb86cbd9
banditakarki <91904380+banditakarki@users.noreply.github.com>
2022-05-05 20:49:22 -0700

    Update main.R

--- TCGA_processing/main.R
+++ TCGA_processing/main.R
@@ -1,16 +1,16 @@
 #source all the functions so they can be accessed and used later
-source("/mnt/ceph/kark6289/TCGA_analysis/trios/trio.code/trios.R")
-source("/mnt/ceph/kark6289/TCGA_analysis/trios/trio.code/findTrioAll.R")
-source("/mnt/ceph/kark6289/TCGA_analysis/trios/trio.code/findDups.R")
-source("/mnt/ceph/kark6289/TCGA_analysis/trios/trio.code/removeNA.R")
-source("/mnt/ceph/kark6289/TCGA_analysis/trios/trio.code/addDups.R")
+source("/mnt/ceph/kark6289/test_trio/trios/trio.code/trios.R")
+source("/mnt/ceph/kark6289/test_trio/trios/trio.code/findTrioAll.R")
+source("/mnt/ceph/kark6289/test_trio/trios/trio.code/findDups.R")
+source("/mnt/ceph/kark6289/test_trio/trios/trio.code/removeNA.R")
+source("/mnt/ceph/kark6289/test_trio/trios/trio.code/addDups.R")
 
 #set the working directory
 setwd("/mnt/ceph/kark6289")
 
 #load the packages
 library("splitstackshape")
-library("org.Hs.eg.db")
+library(org.Hs.eg.db)
 
 #set the directory
 setwd("/mnt/ceph/megheib/Cancer_Genomics/TCGA_data_results")
@@ -43,19 +43,39 @@ dim(meth)
 final <- findTrioAll(meth, cna, gene)
 print(final[1:5,])
 
+colnames(final) <- c("Gene name", "meth.row", "cna.row", "gene.row")
+final.colnames <- colnames(final)
+
+setwd("/mnt/ceph/kark6289/test_trio/trios")
+getwd()
+
+write.table(t(final.colnames), file = "/mnt/ceph/kark6289/test_trio/trios/pre.final.txt", sep = "\t", row.names = FALSE,
+                         col.names = FALSE, append = FALSE,quote=FALSE)
+
+
 #save the trios dataset to the path in txt format
-write.table(final, file = "/mnt/ceph/kark6289/TCGA_analysis/trios/final.copy.txt", sep = "\t", row.names = FALSE,
-                         col.names = TRUE, append = TRUE,quote=FALSE)
+write.table(final, file = "/mnt/ceph/kark6289/test_trio/trios/pre.final.txt", sep = "\t", row.names = FALSE,
+                         col.names = FALSE, append = TRUE,quote=FALSE)
 
 #find the duplicates for genes with multiple entrez ids in CNA data
-final.tmp <- cna.add.dups(final, org.Hs.eg.db)
+final.tmp <- cna.add.dups(final, cna)
 
 final <- final.tmp[[1]]
-dup.final <- final.tmp[[1]]
+print(dim(final))
+
+dup.final <- final.tmp[[2]]
+print(dim(dup.final))
 
 #find the duplicates for genes with multiple entrez ids in Gene Exp data
-final.res <- gene.add.dups(final, dup.final, org.Hs.eg.db)
+final.res <- gene.add.dups(final, dup.final, gene)
+print(final.res[1:10,])
+print(dim(final.res))
+
+write.table(t(colnames(final.res)), file = "/mnt/ceph/kark6289/test_trio/trios/Trios.final.txt", sep = "\t", row.names = FALSE,
+                         col.names = FALSE, append = FALSE,quote=FALSE)
+
 
 #save the data to txt file
-write.table(final.res, file = "/mnt/ceph/kark6289/TCGA_analysis/trios/Trios.final.txt", sep = "\t", row.names = FALSE,
-            col.names = TRUE, append = TRUE,quote=FALSE)
+write.table(final.res, file = "/mnt/ceph/kark6289/test_trio/trios/Trios.final.txt", sep = "\t", row.names = FALSE,
+            col.names = FALSE, append = TRUE,quote=FALSE)
+
