519d83a2eeb2c78bb482ff310fe502dba610d7c7
Bandita Karki <91904380+banditakarki@users.noreply.github.com>
2023-01-09 15:37:59 -0800

    Update findDups.R

--- TCGA_processing/findDups.R
+++ TCGA_processing/findDups.R
@@ -1,3 +1,42 @@
+findDups <- function(data){
+
+  # Finding duplicates in entrez id
+
+  #gives rows for which there are no NA as entrez id
+  nona.ent <- which(!is.na(data$Entrez_Gene_Id))
+
+  #the entrez ids of the above rows
+  nona.entid <- data$Entrez_Gene_Id[nona.ent]
+  nona.entid[1:5]
+
+  #checks if the entred ids have duplicates or not (TRUE/FALSE)
+  d.ent <- duplicated(nona.entid)
+
+  #find which rows have duplicates (gives only the first one not the appearances)
+  dups.rows <- which(d.ent == TRUE)
+
+  #pick the first duplicated row and find the entrez id
+  entz.ids <- nona.entid[dups.rows]
+
+  #initialize variable for duplicated rows in CNA data
+  dups.row.data <- NULL
+
+  #find the rows where this particular entrez id is duplicated
+  for(i in 1:length(entz.ids)){
+
+  #since there are multiple entrez ids, we loop through each one of them
+  dups <- which(data$Entrez_Gene_Id == entz.ids[i])
+
+  #save all the row numbers
+  dups.row.data <- c(dups.row.data, dups)
+  }
+
+  #return the result
+  return(dups.row.data)
+}
+
+
+
 
 findDupsCNA <- function(cna){
 
