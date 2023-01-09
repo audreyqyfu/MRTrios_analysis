18391bb31014a6808506e750fe48eb42a1ba8415
Bandita Karki <91904380+banditakarki@users.noreply.github.com>
2023-01-09 15:29:30 -0800

    Update removeNA.R

--- TCGA_processing/removeNA.R
+++ TCGA_processing/removeNA.R
@@ -1,3 +1,14 @@
+removeNA <- function(data){
+
+   #find the rows that have NA as values for all the individuals in Methylation data
+   na.cols.meth <- which(rowSums(is.na(TCGA.meth[,nStart:ncol(TCGA.meth)])) == ncol(TCGA.meth[,nStart:ncol(TCGA.meth)]))
+
+   #return the result
+   return(na.cols)
+
+}
+
+
 removeNA.meth <- function(TCGA.meth){
 
    #find the rows that have NA as values for all the individuals in Methylation data
