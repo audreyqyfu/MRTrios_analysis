be56cb8c9337c4ad656f27000dffdfd606b31c21
Bandita Karki <91904380+banditakarki@users.noreply.github.com>
2023-06-27 14:45:38 -0700

    Update DataProcessing.R
    updated code and comments

--- DataProcessing.R
+++ DataProcessing.R
@@ -1,5 +1,5 @@
 
-########## histograms ###########
+########## histograms for few methylation probes ###########
 
 pdf(file = "/mnt/ceph/kark6289/TCGA_analysis/newhist/unscaled/TCGA.pdf", onefile=TRUE)
 
@@ -7,7 +7,7 @@ for(i in 1:100) {
   
   print(i)
   
-  data.nona <- log(data[,5:899]/(1-data[,5:899]))
+  data.nona <- log(data[i,5:899]/(1-data[i,5:899]))
   
   hist(as.numeric(data.nona), xlim = c(-2,3.5), main = paste("Histogram of (TCGA) ",data$Row.names[i], sep = ""), xlab = "Methylation")
   
@@ -27,7 +27,6 @@ data <- TCGA.meth[rowSums(is.na(TCGA.meth[,5:899])) != ncol(TCGA.meth[,5:899]), 
 dim(data)
 
 data.nona <- log(data[,5:899]/(1-data[,5:899]))
-
 
 # split data into numbers and probe info
 data.only <- t(data[,-(1:4)])
@@ -74,7 +73,7 @@ if(tmp$`ER Status By IHC`[row.num] == "Positive"){
   
   write.table(t(ER.pos), file = "/mnt/ceph/kark6289/TCGA_analysis/ERpos.csv", sep = ",", row.names = FALSE,
               col.names = FALSE, append = TRUE,quote=FALSE)
-}else{
+}else if(tmp$`ER Status By IHC`[row.num] == "Negative"){
   
   ER.neg <- c(new.rowname,data.only[i,],tmp$`ER Status By IHC`[row.num])
   write.table(t(ER.neg), file = "/mnt/ceph/kark6289/TCGA_analysis/ERneg.csv", sep = ",", row.names = FALSE,
@@ -83,55 +82,6 @@ if(tmp$`ER Status By IHC`[row.num] == "Positive"){
 }
 }
 }
-
-#######################################
-
-setwd("/mnt/ceph/megheib/Cancer_Genomics/TCGA_data_results")
-getwd()
-
-load("TCGA.meth.RData")
-dim(TCGA.meth)
-
-data <- TCGA.meth[rowSums(is.na(TCGA.meth[,5:899])) != ncol(TCGA.meth[,5:899]), ]
-
-dim(data)
-data[1:5,1:5]
-
-# split data into numbers and probe info
-data.only <- t(data[,-(1:4)])
-data.info <- data[,1:4]
-rm(data)
-
-final.col.num = NULL
-
-#split individual ID and match
-for(i in 1:nrow(data.only)){
-  
-split.ID <- unlist(strsplit(as.character(rownames(data.only)[i]), '-'))
-split.ID
-
-#now use this ID to search in the clinical data
-new.rowname <- paste(split.ID[1],"-",split.ID[2],"-",split.ID[3],"-","01", sep = "")
-new.rowname
-
-#find the patient ID in the clinical data
-col.num <- which(as.character(new.rowname) == as.character(colnames(cna)))
-col.num
-
-final.col.num <- c(final.col.num,col.num)
-
-}
-
-#match gene names
-tmp <- na.omit(match(data$Gene_Symbol,cna$Hugo_Symbol))
-
-
-# res <- cna[tmp,]
-# final.res <- res[,final.col.num]
-
-write.table(final, file = "/mnt/ceph/kark6289/TCGA_analysis/split.names.TCGA.meth.logit.txt", sep = ",", row.names = FALSE,
-            col.names = TRUE, append = TRUE,quote=FALSE)
-
 
 
 
