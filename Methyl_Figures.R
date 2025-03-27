6e7d943e42a507952cabcb7c4ffceb89013fca5c
LianZuo <112709486+LianZuo8197@users.noreply.github.com>
2025-03-26 22:58:00 -0400

    Update Methyl_Figures.R

--- Methyl_Figures.R
+++ Methyl_Figures.R
@@ -443,3 +443,168 @@ summary(M0.1_neg$Gene...GC.content)
 summary(M0.2_neg$Gene...GC.content)
 summary(M1.1_neg$Gene...GC.content)
 summary(M1.2_neg$Gene...GC.content)
+
+##################################
+#
+# Location_AllmodelType Figure #
+#
+##################################
+
+
+######## 1. Load data files and packages  #########
+#set the directory
+library(data.table)
+setwd("/Users/lianzuo/LZ/WayneSU/raw_Data_Methyl")
+#read in the datasets
+HumanMethInfo <- read.csv("GPL13534_HumanMethylation450_15017482_v.1.1 2.csv",skip = 7, header = TRUE)
+dim(HumanMethInfo)
+trios <- fread("trio.final.protein.coding.txt", data.table=F)
+TCGA_meth <- fread("split.names.TCGA.meth.logit.txt", data.table=F)
+
+#TC0 <- TCGA_meth%>%dplyr::select(Name=Row.names)
+#df2 <- df%>%dplyr::select(Name, UCSC_RefGene_Group) ## 486428 rows and unique-485,592
+#df2 <- df2%>%dplyr::filter(Name%in%TC0$Name) ##485,577 rows and unique-485,577
+#TC0 <- TC0%>%left_join(df2, by="Name") ## Row.name and Loc 
+
+####### 2. clean data #########
+
+# find the meth rows in trios
+trios_methrow <- trios$meth.row
+#sort the meth data with the same rows in trios
+meth_trio <- TCGA_meth[trios_methrow,]
+# define the Rownames in sorted meth data as Name, using for merge later.
+Name <- meth_trio$Row.names
+# add the Name to tiros data
+trios_add_Name=cbind(trios,Name)
+head(trios_add_Name)
+# add the trio rows as a column 
+trios_add_Name$trios_row=c(1:nrow(trios_add_Name))
+
+#select the two coloums in HumanMethInfo data,it is easy to calculate
+info_1 <- HumanMethInfo %>% select(Name,UCSC_RefGene_Group)
+# filter the matched Rownames in df_1 data
+info_2 <- info_1 %>% filter(Name %in% trios_add_Name$Name)
+#merge two dataframes together by the same Name
+merge_df <- merge(trios_add_Name,info_2,by="Name",all=F)
+####merge_df <- left_join(trios_add_Name,info_2,by="Name")
+#sort the data in trios_row order(from 1 to 295492)
+merge_trio_loc=merge_df %>% arrange(trios_row)
+head(merge_trio_loc)
+#df data using the v1.1 version,the problem is: trios_add_Name : count(Name)=295492,but has duplicates,there are 29462 Names not match in df_2 Name;
+#df_2 :count(Name)=246070, no duplicates,the final location csv:nrow()=266030 .not equal to nrow(trios)
+# 29462 Rownames in meth data are not in df(humanmethylation) data,that means they don't has UCSC_RefGene_Group data.
+#trios[(221:230,1200:1219),]
+# df data using the v1.2 version, the rownames in meth data are all in df(Humanmethylation) data.
+dim(merge_df)# [295492,2]
+
+table(is.na(match(trios_add_Name$Name,info_2$Name)))
+#before:TRUE :29462(not match names) #after:FALSE :295492(all matched)
+
+##create a summary table for the location
+table(unlist(strsplit(as.character(merge_trio_loc$UCSC_RefGene_Group), ';')))
+
+#write final to the location file
+write.table(merge_trio_loc, file = "/Users/lianzuo/LZ/WayneSU/Methyl_Analysis_LZ/Data_Result_Methyl_LZ/final_protein_coding_location_LZ_03_19_25.csv", sep = "\t", row.names = FALSE,col.names = T, append = F, quote=FALSE)
+
+# load the data
+
+
+######## 3/16/2025 ###########
+loc <- fread("/Users/lianzuo/LZ/WayneSU/Methyl_Analysis_LZ/Data_Result_Methyl_LZ/final_protein_coding_location_LZ_03_19_25.csv",data.table = F)
+pos_modeltype<-read.delim('/Users/lianzuo/LZ/WayneSU/raw_Data_Methyl/model.trio.MRGN.all.posER.reclassify2.txt')
+#combine the pos modeltype with the location infos by trio_row
+merge_pos_df=left_join(pos_modeltype,loc,by=c("trio_row"="trios_row"))
+pos_df=merge_pos_df %>% separate_rows(UCSC_RefGene_Group, sep = ";")
+
+#M1.1=dff_1 %>% filter(Inferred.Model3=="M1.1")
+#table(M1.1$UCSC_RefGene_Group)
+
+## Combine the 6 locations into 3
+pos_df_2=pos_df %>% mutate(Group= case_when(
+  UCSC_RefGene_Group %in% c("TSS200","TSS1500") ~ "TSS",
+  UCSC_RefGene_Group %in% c("Body","1stExon") ~ "Body",
+  UCSC_RefGene_Group %in% c("3'UTR","5'UTR") ~ "5'/3'UTR",
+  TRUE ~NA_character_))
+## barplot
+#ggplot(dff_2)+geom_bar(aes(x=Inferred.Model3,fill=Group,color=Group),position = position_dodge(0.8),width = 0.7)
+#+theme_bw()+theme(legend.position = "right")
+#+labs(title = "ER+",x="Inferrence Models",y="Count (thousands)")
+#+scale_y_continuous(limits = c(0, 180000), labels=seq(0,15,5),breaks = seq(0, 150000, 50000))
+
+
+pos_df_counts <- pos_df_2 %>%
+  group_by(Inferred.Model3, Group) %>%
+  summarise(Count = n(), .groups = 'drop')
+table(dff_2$Group)
+df_counts$amount=rep(c(121627,362129,191201),9)
+df_counts$percent=round((df_counts$Count/df_counts$amount)*100,digits = 1)
+
+# Create the bar plot (Count)
+ggplot(pos_df_counts, aes(x = Inferred.Model3, y = Count, fill = Group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "(ER+) Count of Locations by Model Type",x = "Model Type",y = "Count (thousand)",fill = "Group") +
+  theme_bw()+scale_y_continuous(limits = c(0, 180000), labels=seq(0,15,5),breaks = seq(0, 150000, 50000))+
+  theme(legend.position = "top",plot.title = element_text(size=14,hjust = 0.5))+
+  scale_fill_manual(name = "Location",  values = c("TSS" = "#377eb8", "Body" = "#4daf4a", "5'/3'UTR" = "#e7298a"))  # Optional: Customize colors)
+
+#bar plot _ log10(Count) 
+ggplot(pos_df_counts, aes(x = Inferred.Model3, y = log10(Count), fill = Group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "(ER+) Count of Locations by Model Type",x = "Model Type",y = "log10(Count)",fill = "Group") +
+  theme_bw()+scale_y_continuous(limits = c(0, 6), labels=seq(0,6,1),breaks = seq(0, 6, 1))+
+  theme(legend.position = "top",plot.title = element_text(size=14,hjust = 0.5))+
+  scale_fill_manual(name = "Location",  values = c("TSS" = "#377eb8", "Body" = "#4daf4a", "5'/3'UTR" = "#e7298a"))  # Optional: Customize colors)
+
+# Create the bar plot (Percentage)
+ggplot(pos_df_counts, aes(x = Inferred.Model3, y = percent, fill = Group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "( ER+ ) Count of Locations by Model Type",x = "Model Type",y = "Percentage",fill = "Group") +theme_bw()+
+  theme(legend.position = "top",plot.title = element_text(size=14,hjust = 0.5))+
+  scale_fill_manual(name = "Location",  # Change the legend title here
+                    values = c("TSS" = "#377eb8", "Body" = "#4daf4a", "5'/3'UTR" = "#e7298a")  # Optional: Customize colors
+  )
+##add labels to a dodged barplot
+#+geom_text(aes(label=Count,group=Group),position = position_dodge(0.8),vjust=-0.3,size=3.5)
+
+
+##### neg ######
+
+neg_df0 <-read.delim('/Users/lianzuo/LZ/WayneSU/raw_Data_Methyl/model.trio.MRGN.all.negER.reclassify2.txt')
+neg_df=left_join(neg_df0,loc,by=c("trio_row"="trios_row"))
+neg_dff_2=neg_df %>% separate_rows(UCSC_RefGene_Group, sep = ";")
+
+## Combine the 6 locations into 3
+neg_dff_2=neg_dff_2 %>% mutate(Group= case_when(
+  UCSC_RefGene_Group %in% c("TSS200","TSS1500") ~ "TSS",
+  UCSC_RefGene_Group %in% c("Body","1stExon") ~ "Body",
+  UCSC_RefGene_Group %in% c("3'UTR","5'UTR") ~ "5'/3'UTR",
+  TRUE ~NA_character_))
+
+neg_df_counts <- neg_dff_2 %>%
+  group_by(Inferred.Model3, Group) %>%
+  summarise(Count = n(), .groups = 'drop')
+
+table(neg_dff_2$Group)
+neg_df_counts$amount=rep(c(121627,362079,191129),9)
+neg_df_counts$percent=round((neg_df_counts$Count/neg_df_counts$amount)*100,digits = 1)
+
+# ER- Create the bar plot
+# Create the bar plot
+ggplot(neg_df_counts, aes(x = Inferred.Model3, y = Count, fill = Group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "ER- :Count of Locations by Model Type",x = "Model Type",y = "Count (thousand)",fill = "Group") +
+  theme_bw()+scale_y_continuous(limits = c(0, 150000), labels=seq(0,15,5),breaks = seq(0, 150000, 50000))+
+  theme(legend.position = "top",plot.title = element_text(size=14,hjust = 0.5))+
+  scale_fill_manual(name = "Location",  # Change the legend title here
+                    values = c("TSS" = "#377eb8", "Body" = "#4daf4a", "5'/3'UTR" = "#e7298a")  # Optional: Customize colors
+  )
+
+# Create the bar plot(Pecentage)
+ggplot(neg_df_counts, aes(x = Inferred.Model3, y = percent, fill = Group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "( ER- ) Count of Locations by Model Type",x = "Model Type",y = "Percentage",fill = "Group") +theme_bw()+
+  theme(legend.position = "top",plot.title = element_text(size=14,hjust = 0.5))+
+  scale_fill_manual(name = "Location",  # Change the legend title here
+                    values = c("TSS" = "#377eb8", "Body" = "#4daf4a", "5'/3'UTR" = "#e7298a")  # Optional: Customize colors
+  )
+
